520424910a32727603c9d8067a86ecbb0aa7d658
MiniPaul <minipaul1227@gmail.com>
2024-03-11 15:38:17 +0530

    Development branch created

--- src/Components/ViewmyComplaint.jsx
+++ src/Components/ViewmyComplaint.jsx
@@ -44,7 +44,7 @@ const ViewmyComplaint = () => {
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <div className="row g-3">
-                <div className="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <br /><br /><h1><center>My Complaints</center></h1>
                     <div>
                     {
